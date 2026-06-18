e5ed17da65a48cf63d48fff876744f39ca7129a2
khushi_bodakhe <khushibodakhe10@gmail.com>
2026-06-19 01:00:48 +0530

    Add files via upload

--- db_connection.py
+++ db_connection.py
@@ -1,12 +1,21 @@
 # db_connection.py
 
-import mysql.connector
+#import mysql.connector
 
-def get_connection():
-    conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="khushi@2004",
-        database="marketplace_analytics"
-    )
-    return conn
+#def get_connection():
+#    conn = mysql.connector.connect(
+ #       host="localhost",
+  #      user="root",
+   #     password="khushi@2004",
+    #    database="marketplace_analytics"
+   # )
+    #return conn 
+    
+import pandas as pd
+    
+students= pd.read_csv("data/students.csv")
+jobs= pd.read_csv("data/jobs.csv")
+companies= pd.read_csv("data/companies.csv")
+interviews= pd.read_csv("data/interviews.csv")
+    
+    
